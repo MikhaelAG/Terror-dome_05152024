eca2309ecab8227d47c58162a6586590bc432749
MikhaelAG <130944619+MikhaelAG@users.noreply.github.com>
2024-05-16 23:14:25 -0500

    Add 02-Networks.tf

--- 02-Networks.tf
+++
@@ -1,9 +0,0 @@
-# Networks - Europe (HQ)/Americas/Asia-Pacific
-module "vpc_networks" {
-  for_each = var.networks
-
-  source                  = "./modules/vpcs/vpc_network"
-  name                    = each.value.name
-  project                 = each.value.project
-  auto_create_subnetworks = false
-}
